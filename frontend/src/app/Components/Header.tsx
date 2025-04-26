48795337ada4b420e2f077edf5306647d48a9c19
Jujej <jpdurau@gmail.com>
2025-04-26 19:00:34 -0300

    Implementação da página de aulas

--- frontend/src/app/Components/Header.tsx
+++ frontend/src/app/Components/Header.tsx
@@ -23,19 +23,24 @@ const AuthenticatedHeader = () => {
         <>
             {
                 <>
-                    <div className="w-full shadow flex flex-col gap-1 items-center justify-between px-18 pb-4 md:flex-row md:pb-0">
-                        <p className="logo text-purple"><span className="text-[60px] font-black">XP</span>Meister</p>
-                        <div className="flex flex-row text-[18px] gap-10">
+                    <div className="w-full shadow flex flex-col gap-3 items-center bg-purple justify-between px-18 pb-4  md:flex-row md:pb-0">
+
+                        <p className="logo text-white"><span className="text-[60px] font-black">XP</span>Meister</p>
+                        <div className="flex items-center text-white flex-row text-[18px] gap-10">
                             <a href="*" className="font-semibold">Sobre</a>
                             <a href="*" className="font-semibold">Contato</a>
-                            <a href="*" className="font-semibold">Entrar</a>
+                            <a href="*" className="font-semibold bg-white p-[2px] rounded-full">
+                                <img src="/UserIcon.png" alt="UserIcon" 
+                                className="min-w-[30px] min-h[30px] max-w-[40px] max-h-[40px]"/>
+                            </a>
                         </div>
+
                     </div>
-                </>
+                    </>
 
                 //TODO Implementar header logado
             }
-        </>
+                </>
     )
 }
 
@@ -51,4 +56,4 @@ const Header = () => {
 
 
 
-export default Header;
+            export default Header;
