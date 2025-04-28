31b296538d570708fd0df5538398497b8aab911b
ZFidelis <isaaczapo@hotmail.com>
2025-04-28 20:48:51 -0300

    change return message

--- Backend/Model/Usuario.cs
+++ Backend/Model/Usuario.cs
@@ -8,13 +8,13 @@ namespace Backend.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; private set; }
-        [Required(ErrorMessage = "O campo é obrigatório.")]
-        public string Nome { get; set; }
-        [Required(ErrorMessage = "O campo é obrigatório.")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "O campo é obrigatório.")]
-        public string Senha { get; set; }
-        public string Role { get; private set; }
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Email é obrigatório.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Senha é obrigatório.")]
+        public string? Senha { get; set; }
+        public string? Role { get; private set; }
         public bool Ativo { get; private set; }
 
         public Usuario()
