b224f71dc7a9f717cae630feccd3322f64495260
gustavogogola91 <gustavogogola3@gmail.com>
2025-04-30 17:16:25 -0300

    Adicionado useState para mudança do formulario

--- frontend/src/app/Pages/AlunoConfigPage.tsx
+++ frontend/src/app/Pages/AlunoConfigPage.tsx
@@ -1,18 +1,44 @@
+"use client";
+
+import { useState } from "react";
+
 export default function AlunoConfigPage() {
+  const [configPage, setConfigPage] = useState({ page: "senha" });
+
   return (
     <div className="flex flex-1">
       <aside className="hidden md:inline w-64 my-12 border-r border-purple-500">
         <ul>
-          <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+          <li
+            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              setConfigPage({ page: "senha" });
+            }}
+          >
             Alterar Senha
           </li>
-          <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+          <li
+            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              setConfigPage({ page: "email" });
+            }}
+          >
             Alterar Email
           </li>
-          <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+          <li
+            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              setConfigPage({ page: "estudo" });
+            }}
+          >
             Personalizar estudo
           </li>
-          <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+          <li
+            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              setConfigPage({ page: "modulo" });
+            }}
+          >
             Refazer módulo
           </li>
         </ul>
@@ -23,7 +49,16 @@ export default function AlunoConfigPage() {
           Configurações
         </h2>
 
+        <Formulario />
+      </main>
+    </div>
+  );
+
+  function Formulario() {
+    if (configPage.page == "senha") {
+      return (
         <form className="mx-auto w-full max-w-md flex flex-col items-center space-y-6">
+          <h3 className="uppercase text-purple font-bold">Alterar senha</h3>
           <div className="w-full">
             <label className="block text-center mb-2 text-sm">
               Insira a senha atual
@@ -46,7 +81,7 @@ export default function AlunoConfigPage() {
 
           <div className="w-full">
             <label className="block text-center mb-2 text-sm">
-              Insira a nova senha
+              Confirme a senha
             </label>
             <input
               type="password"
@@ -61,7 +96,49 @@ export default function AlunoConfigPage() {
             Alterar senha
           </button>
         </form>
-      </main>
-    </div>
-  );
+      );
+    } else if (configPage.page == "email") {
+      return (
+        <form className="mx-auto w-full max-w-md flex flex-col items-center space-y-6">
+          <h3 className="uppercase text-purple font-bold">Alterar Email</h3>
+          <div className="w-full">
+            <label className="block text-center mb-2 text-sm">
+              Insira a senha atual
+            </label>
+            <input
+              type="password"
+              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+            />
+          </div>
+
+          <div className="w-full">
+            <label className="block text-center mb-2 text-sm">
+              Insira o novo email
+            </label>
+            <input
+              type="email"
+              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+            />
+          </div>
+
+          <div className="w-full">
+            <label className="block text-center mb-2 text-sm">
+              Confirme o email
+            </label>
+            <input
+              type="email"
+              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded shadow transition"
+          >
+            Alterar senha
+          </button>
+        </form>
+      );
+    }
+  }
 }
