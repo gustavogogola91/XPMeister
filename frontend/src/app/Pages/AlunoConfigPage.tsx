8a3d1611758530019a3761cfb7cbe203351756f6
gustavogogola91 <gustavogogola3@gmail.com>
2025-05-01 10:15:02 -0300

    Implementando lógica para requisição backend WIP

--- frontend/src/app/Pages/AlunoConfigPage.tsx
+++ frontend/src/app/Pages/AlunoConfigPage.tsx
@@ -2,8 +2,19 @@
 
 import { useState } from "react";
 
+var apiUrl = "http://localhost:5017";
+
 export default function AlunoConfigPage() {
   const [configPage, setConfigPage] = useState({ page: "senha" });
+  const [alteracao, setAlteracao] = useState({
+    senha: "",
+    novo: "",
+    confirma: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAlteracao({ ...alteracao, [name]: value });
+  };
 
   return (
     <div className="flex flex-1">
@@ -49,96 +60,173 @@ export default function AlunoConfigPage() {
           Configurações
         </h2>
 
-        <Formulario />
+        <Page
+          configPage={configPage}
+          alteracao={alteracao}
+          handleChange={handleChange}
+          alterarSenha={alterarSenha}
+        />
       </main>
     </div>
   );
 
-  function Formulario() {
-    if (configPage.page == "senha") {
-      return (
-        <form className="mx-auto w-full max-w-md flex flex-col items-center space-y-6">
-          <h3 className="uppercase text-purple font-bold">Alterar senha</h3>
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Insira a senha atual
-            </label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
+  async function alterarSenha() {
+    if (alteracao.confirma === alteracao.senha) {
+      var novo = alteracao.novo;
 
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Insira a nova senha
-            </label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
+      try {
+        const response = await fetch(`${apiUrl}/usuario/1`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Senha: novo }),
+        });
 
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Confirme a senha
-            </label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded shadow transition"
-          >
-            Alterar senha
-          </button>
-        </form>
-      );
-    } else if (configPage.page == "email") {
-      return (
-        <form className="mx-auto w-full max-w-md flex flex-col items-center space-y-6">
-          <h3 className="uppercase text-purple font-bold">Alterar Email</h3>
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Insira a senha atual
-            </label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
-
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Insira o novo email
-            </label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
-
-          <div className="w-full">
-            <label className="block text-center mb-2 text-sm">
-              Confirme o email
-            </label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded shadow transition"
-          >
-            Alterar senha
-          </button>
-        </form>
-      );
+        console.log(response)
+      } catch (error) {
+        return null;
+      }
     }
+  }
+
+  async function alterarEmail() {
+    if (alteracao.confirma === alteracao.novo) {
+      var novo = alteracao.novo;
+
+      try {
+        const response = await fetch(`${apiUrl}/usuario/`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Email: novo }),
+        });
+      } catch (error) {
+        return null;
+      }
+    }
+  }
+}
+
+function Page({
+  configPage,
+  alteracao,
+  handleChange,
+  alterarSenha,
+}: {
+  configPage: { page: string };
+  alteracao: { senha: string; novo: string; confirma: string };
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  alterarSenha: () => void;
+}) {
+  if (configPage.page == "senha") {
+    return (
+      <form
+        className="mx-auto w-full max-w-md flex flex-col items-center space-y-6"
+        onSubmit={(e) => {
+          e.preventDefault();
+          alterarSenha();
+        }}
+      >
+        <h3 className="uppercase text-purple font-bold">Alterar senha</h3>
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Insira a senha atual
+          </label>
+          <input
+            type="password"
+            name="senha"
+            value={alteracao.senha}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Insira a nova senha
+          </label>
+          <input
+            type="password"
+            name="novo"
+            value={alteracao.novo}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Confirme a senha
+          </label>
+          <input
+            type="password"
+            name="confirma"
+            value={alteracao.confirma}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded shadow transition"
+        >
+          Alterar senha
+        </button>
+      </form>
+    );
+  } else if (configPage.page == "email") {
+    return (
+      <form className="mx-auto w-full max-w-md flex flex-col items-center space-y-6">
+        <h3 className="uppercase text-purple font-bold">Alterar Email</h3>
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Insira a senha atual
+          </label>
+          <input
+            type="password"
+            name="senha"
+            value={alteracao.senha}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Insira o novo email
+          </label>
+          <input
+            type="email"
+            name="novo"
+            value={alteracao.novo}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <div className="w-full">
+          <label className="block text-center mb-2 text-sm">
+            Confirme o email
+          </label>
+          <input
+            type="email"
+            name="confirma"
+            value={alteracao.confirma}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded shadow transition"
+        >
+          Alterar senha
+        </button>
+      </form>
+    );
   }
 }
